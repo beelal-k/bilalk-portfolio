fb376bf1c3b748a228d6cdc67a994c75a033ca2d
bilal-kun <khawajabilal902@gmail.com>
2022-11-05 18:29:23 +0500

    remade the project on a new next-app cuz webpack probs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
+
+import React, { useEffect, useState } from 'react'
 import '../styles/globals.css'
+import { JetBrains_Mono } from '@next/font/google'
+
+const jetbrains_mono = JetBrains_Mono();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+  return (
+    <>
+      <style jsx global>
+        {`
+        html {
+          font-family: ${jetbrains_mono.style.fontFamily};
+        }
+      `}
+      </style>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
